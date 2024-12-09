6714fc2ee4a497d8f1dbc96fcdc9f8094142e7b6
DallanL <dallan@wedophones.com>
2024-12-09 21:11:59 +0000

    added subscription handling

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="netsapiens-asyncio",
-    version="0.1.0",
+    version="0.1.1",
     description="Asynchronous Python client library for the Netsapiens PBX API",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
